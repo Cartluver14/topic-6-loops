558357f2583ca24500fa9025395923edd9142d5b
339713067 <339713067@2260I-L29-023>
2025-10-01 09:38:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace topic_6_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = "";
+
+            while (choice != "q")
+            {
+                Console.Clear(); // Optional
+                Console.WriteLine("Welcome to my generic menu.  Please select an option:");
+                Console.WriteLine();
+                Console.WriteLine("1 - Menu Option 1");
+                Console.WriteLine("2 - Menu Option 2");
+                Console.WriteLine("...");
+                Console.WriteLine("x - Menu Option x");
+                Console.WriteLine("Q - Quit");
+                Console.WriteLine();
+                var input = Console.ReadLine();
+                choice = input == null ? "q" : input.ToLower().Trim();
+                Console.WriteLine();
+
+                if (choice == "1")
+                {
+                    //Do option 1
+                    Console.WriteLine("You chose option 1");
+                   
+                    Console.Write("Enter the minimum value");
+                    int min = int.Parse(Console.ReadLine());
+
+                   
+                    Console.Write("Enter the maximum value");
+                    int max = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine();("Enter a number between min and max");
+
+                    Console.WriteLine($"You entered Min = {min} and Max = {max}");
+                }
+                else if (choice == "2")
+                {
+                    // Do option 2
+                    Console.WriteLine("You chose option 2");
+                    Console.WriteLine("Hit ENTER to continue.");
+                    Console.ReadLine();
+                }
+                // Add an else if for each valid choice...
+                else if (choice != "q")
+                {
+                    Console.WriteLine("Invalid choice, press ENTER to continue.");
+                    Console.ReadLine();
+                }
+            }
+        }
+}
+}
